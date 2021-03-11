9985a64131c66acbf80f5115073184515ddf60bd
Eunseok Song <cnsong0229@gmail.com>
2021-03-12 01:37:12 +0900

    Update ExampleSend.java

--- miniproject_swing_parkking/carParkingRef/sms/SMS/src/ExampleSend.java
+++ miniproject_swing_parkking/carParkingRef/sms/SMS/src/ExampleSend.java
@@ -5,14 +5,14 @@ import net.nurigo.java_sdk.exceptions.CoolsmsException;
 
 public class SendTextMessage {
   public void messageGo(String carNum, String phoneNumber, int parkingCode){
-    String api_key = "NCS4XMLZAZHD0FGM";
-    String api_secret = "OCYRGRWRQRMH4MHCNKGO9OFRWALE9SLA";
+    String api_key = ""; //원래 내용있어야함
+    String api_secret = ""; //위와 동일
     Message coolsms = new Message(api_key, api_secret);
 String message = "차량번호 " + carNum+ "님의 주차 코드는 " +parkingCode + "입니다."; 
     // 4 params(to, from, type, text) are mandatory. must be filled
     HashMap<String, String> params = new HashMap<String, String>();
     params.put("to", phoneNumber);
-    params.put("from", "01063735786"); //무조건 자기번호 (인증)
+    params.put("from", "번호"); //무조건 자기번호 (인증)
     params.put("type", "SMS");
     params.put("text", message);
     params.put("app_version", "test app 1.2"); // application name and version
